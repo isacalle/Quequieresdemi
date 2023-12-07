ff7c9bb93890ca13a50b87872038f4b9f9245ebe
isacalle <isabelacalle.dev@gmail.com>
2023-12-06 22:19:12 -0500

    primer commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let primerNombre = "Laura";
+let segundoNombre = "Bea";
+
+let usuario= prompt("ingrese su usuario");
+
+let contraseña= prompt("ingrese su contraseña");
+
+if (usuario == "Laura") {
+   alert("Hola " + usuario + "," + "¡Bienvenidx!"); 
+} else if (usuario == "Bea"){
+    alert("Hola " + usuario + "," + "¡Bienvenidx!"); 
+} else {
+    alert("Usuario incorrecto")
+}
+
+
+console.log(usuario, contraseña);
+
+const BROCHAS = 12;
+
+const CORRECTOR = 10;
+
+const RUBOR = 5;
+
+const SOMBRAS = 6;
+
+const GANCHO = 3;
+
+const LABIAL = 4;
+
+const ILUMINADOR = 5;
+
+const LABIALESS = 4;
+
